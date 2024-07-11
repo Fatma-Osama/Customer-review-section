b309990cde0179457fe59586e77fc66a40db30f6
Fosama <fosama@futureface.sa>
2024-07-11 23:41:58 +0300

    customer reviews

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+let progressList = document.querySelectorAll('.rating-progress')
+
+for(let n of progressList){
+  let progress = n.querySelector(".progress")
+  let proPercentage = n.querySelector(".progress-percentage")
+  progress.style.width= proPercentage.textContent
+}
+
+
+
+
